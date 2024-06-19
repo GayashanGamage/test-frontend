34c71070998a00bbd8db5d56075ae34b48201333
gayashan <gayashan.randimagamage@gmail.com>
2024-06-19 14:31:40 +0530

    add new button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="level-one-container">
     <p>main page</p>
+    <button @click="clicked">Send request</button>
   </div>
 </template>
 
 <script setup>
 name: "App";
+import axios from "axios";
+
+const clicked = () => {
+  axios
+    .get("https://test-backend-38j6.onrender.com/")
+    .then(function (response) {
+      console.log(response);
+    });
+};
 </script>
